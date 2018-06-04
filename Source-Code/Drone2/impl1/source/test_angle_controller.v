5a8c4205fa273c4d8b98827a5623603c39dca6b7
ethangrinnell78 <ethangrinnell78@users.noreply.github.com>
2018-06-03 22:10:09 -0700

    Update test_angle_controller.v

--- Source-Code/Drone2/impl1/source/test_angle_controller.v
+++ Source-Code/Drone2/impl1/source/test_angle_controller.v
@@ -3,39 +3,145 @@
  * Team 32 Drone2 SOC
  * Ethan Grinnell, Brett Creeley, Daniel Christiansen, Kirk Hooper, Zachary Clark-Williams
  */
+ 
+`timescale 1ps / 1ps
+`default_nettype none
+`include "common_defines.v"
 
-module test_value_to_rate;
+module test_angle_controller;
 
-	localparam RATE_BIT_WIDTH = 36;
-	localparam VAL_BIT_WIDTH = 14;
+	// PWM from receiver module
+	reg signed [`REC_VAL_BIT_WIDTH-1:0] throttle_target = 0;
+	reg signed [`REC_VAL_BIT_WIDTH-1:0] yaw_target      = 0;
+	reg signed [`REC_VAL_BIT_WIDTH-1:0] roll_target     = 0;
+	reg signed [`REC_VAL_BIT_WIDTH-1:0] pitch_target    = 0;
 
-	// target rates (rad/s) in 2's complement fixed point
-	wire [RATE_BIT_WIDTH-1:0] throttle_rate = 0;
-	wire [RATE_BIT_WIDTH-1:0] yaw_rate = 0;
-	wire [RATE_BIT_WIDTH-1:0] roll_rate = 0;
-	wire [RATE_BIT_WIDTH-1:0] pitch_rate = 0;
+	// IMU euler angles
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] yaw_actual   = 0;
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] pitch_actual = 0;
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] roll_actual  = 0;
+	
+	// IMU linear acceleration vectors
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] x_linear_accel = 0;
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] y_linear_accel = 0;
+	reg signed [`IMU_VAL_BIT_WIDTH-1:0] z_linear_accel = 0;
+	
+	wire signed [`RATE_BIT_WIDTH-1:0] throttle_rate_out;
+	wire signed [`RATE_BIT_WIDTH-1:0] yaw_rate_out;
+	wire signed [`RATE_BIT_WIDTH-1:0] pitch_rate_out;
+	wire signed [`RATE_BIT_WIDTH-1:0] roll_rate_out;
+	
+	wire signed [`RATE_BIT_WIDTH-1:0] pitch_angle_error;
+	wire signed [`RATE_BIT_WIDTH-1:0] roll_angle_error;
+	
+	reg  resetn;
+	wire sys_clk;
+	wire us_clk;
+	wire complete_signal;
+	wire active_signal;
+	reg  start_signal;
+	
 
-	// values ranging from -9000 to 9000
-	wire [VAL_BIT_WIDTH-1:0] throttle_val = 0;
-	wire [VAL_BIT_WIDTH-1:0] yaw_val = 0;
-	wire [VAL_BIT_WIDTH-1:0] roll_val = 0;
-	wire [VAL_BIT_WIDTH-1:0] pitch_val = 0;
+	defparam OSCH_inst.NOM_FREQ = "38.00";
+	OSCH OSCH_inst (.STDBY(1'b0),
+       			    .OSC(sys_clk),
+       			    .SEDSTDBY());
 
-	wire sys_clk = 0;
+	us_clk us_clk_divider (
+		.us_clk(us_clk),
+		.sys_clk(sys_clk),
+		.resetn(resetn));
 
 	// line up the parameters here to the ones internal to the receiver module
-	value_to_rate #(RATE_BIT_WIDTH, VAL_BIT_WIDTH) DUT (.throttle_rate(throttle_rate),
-														.yaw_rate(yaw_rate),
-														.roll_rate(roll_rate),
-														.pitch_rate(pitch_rate),
-														.throttle_val(throttle_val),
-														.yaw_val(yaw_val),
-														.roll_val(roll_val),
-														.pitch_val(pitch_val),
-														.sys_clk(sys_clk));
+	angle_controller DUT (
+		.throttle_rate_out(throttle_rate_out),
+		.yaw_rate_out(yaw_rate_out),
+		.pitch_rate_out(pitch_rate_out),
+		.roll_rate_out(roll_rate_out),
+		.pitch_angle_error(pitch_angle_error),
+		.roll_angle_error(roll_angle_error),
+		.complete_signal(complete_signal),
+		.active_signal(active_signal),
+		.throttle_target(throttle_target),
+		.yaw_target(yaw_target),
+		.pitch_target(pitch_target),
+		.roll_target(roll_target),
+		.yaw_actual(yaw_actual),
+		.pitch_actual(pitch_actual),
+		.roll_actual(roll_actual),
+		.resetn(resetn),
+		.start_signal(start_signal),
+		.us_clk(us_clk));
+
+			task run_test;
+				input reg [15:0] task_throttle_target;
+				input reg [15:0] task_yaw_target    ;
+				input reg [15:0] task_roll_target   ;
+				input reg [15:0] task_pitch_target  ;
+				input reg [15:0] task_yaw_actual    ;
+				input reg [15:0] task_pitch_actual  ;
+				input reg [15:0] task_roll_actual   ;
+				begin
+					throttle_target = task_throttle_target;
+					yaw_target      = task_yaw_target    ;
+					roll_target     = task_roll_target   ;
+					pitch_target    = task_pitch_target  ;
+					yaw_actual      = task_yaw_actual    ;
+					pitch_actual    = task_pitch_actual  ;
+					roll_actual     = task_roll_actual   ;
+					start_signal    = 1;
+					repeat (2) @(posedge us_clk); start_signal    = 0; repeat (8) @(posedge us_clk);
+					$display("%t: %m Throttle rate out =%d",$time, throttle_rate_out);
+					$display("%t: %m Yaw rate out =%d",$time, yaw_rate_out);
+					$display("%t: %m Pitch rate out =%d",$time, pitch_rate_out);
+					$display("%t: %m Roll rate out =%d",$time, roll_rate_out);
+					$display("%t: %m Pitch angle error =%d",$time, pitch_angle_error);
+					$display("%t: %m Roll angle error =%d",$time, roll_angle_error);
+				end
+			endtask
 
 	initial begin
-		$display("%m successful");
+		$display("%t: %m Reset angle controller", $time);
+		resetn = 1;
+		#10 resetn = 0;
+		#10 resetn = 1;
+		
+		$display("%t: %m Set initial values",$time);
+		run_test(
+		.task_throttle_target(0),
+		.task_yaw_target(0),
+		.task_roll_target(0),
+		.task_pitch_target(0),
+		.task_yaw_actual(0),
+		.task_pitch_actual(0),
+		.task_roll_actual(0)
+		);
+		
+		$display("\n%t: %m Test neutral stick positions, throttle to 50 percent with angles of value 0 from IMU",$time);
+		run_test(
+		.task_throttle_target(125),
+		.task_yaw_target(0),
+		.task_roll_target(0),
+		.task_pitch_target(0),
+		.task_yaw_actual(0),
+		.task_pitch_actual(0),
+		.task_roll_actual(0)
+		);
+
+
+		$display("\n%t: %m Test pitch 140, throttle to 50 percent, 0 linear acceleration, with angles of value 0 from IMU",$time);
+		run_test(
+		.task_throttle_target(125),
+		.task_yaw_target(0),
+		.task_roll_target(0),
+		.task_pitch_target(140),
+		.task_yaw_actual(0),
+		.task_pitch_actual(0),
+		.task_roll_actual(0)
+		);	
+
+		$display("%t: %m Test complete", $time);
+		$stop;
 	end
 
 endmodule
