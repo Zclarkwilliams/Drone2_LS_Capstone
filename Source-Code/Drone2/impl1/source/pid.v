6c25bb17dd8212cbdac8c86073ba4fcc994c3f2a
Zclarkwilliams <zclarkw112@gmail.com>
2018-06-12 15:57:27 -0700

    made pid 32 bit mult and shift to function

--- Source-Code/Drone2/impl1/source/pid.v
+++ Source-Code/Drone2/impl1/source/pid.v
@@ -18,11 +18,11 @@
  `timescale 1ns / 1ns
  `include "common_defines.v"
 
- module pid #(parameter signed [`OPS_BIT_WIDTH-1:0] RATE_MIN = 32'h80000000,
-	 		  parameter signed [`OPS_BIT_WIDTH-1:0] RATE_MAX = 32'h7FFFFFFF,
-			  parameter signed [`OPS_BIT_WIDTH-1:0] K_P = 1,
-			  parameter signed [`OPS_BIT_WIDTH-1:0] K_I = 1,
-			  parameter signed [`OPS_BIT_WIDTH-1:0] K_D = 1,
+ module pid #(parameter signed [15:0] RATE_MIN = 16'h8000,
+	 		  parameter signed [15:0] RATE_MAX = 16'h7FFF,
+			  parameter signed [15:0] K_P = 1,
+			  parameter signed [15:0] K_I = 1,
+			  parameter signed [15:0] K_D = 1,
 			  parameter signed K_P_SHIFT = 4'h4,
 			  parameter signed K_I_SHIFT = 4'h4,
 			  parameter signed K_D_SHIFT = 4'h4)
@@ -39,13 +39,13 @@
 			  input  wire us_clk);
 
 	// working registers
-	reg signed [`OPS_BIT_WIDTH-1:0]
+	reg signed [15:0]//[`OPS_BIT_WIDTH-1:0]
 		error_change, 
 		rotation_total,
 		rotation_error, 
-		angle_err_temp,
-		target_rot_temp,
-		actual_rot_temp,
+		//angle_err_temp,
+		//target_rot_temp,
+		//actual_rot_temp,
 		prev_rotation_error,
 		scaled_rotation,
 		rotation_integral,
@@ -53,10 +53,10 @@
 		rotation_proportional;
 	
 	// PADDING AND EXTEND SIGN BIT TO 32 BITS
-	localparam SHIFT_TO_LSB = 5'd16;
+//	localparam SHIFT_TO_LSB = 5'd16;
 	
 	// state names
-	localparam STATE_BIT_WIDTH = 7;
+	localparam STATE_BIT_WIDTH = 4'd7;
 	localparam [STATE_BIT_WIDTH-1:0]
 		STATE_WAIT     		= 7'b0000001,
 		STATE_EXTEND_32BIT	= 7'b0000010,
@@ -69,7 +69,7 @@
 	reg [STATE_BIT_WIDTH-1:0] state, next_state;
 
 	//Debug wire assign to monitor values on 16 led daughter board
-	assign DEBUG_WIRE = rotation_total[15:0];
+	assign DEBUG_WIRE = rotation_total[`DEBUG_WIRE_BIT_WIDTH-1:0];
 
 	// update state
 	always @(posedge us_clk or negedge resetn) begin
@@ -84,7 +84,7 @@
 		if(!resetn) begin
 			pid_active 		<= `FALSE;
 			pid_complete 	<= `FALSE;
-			rate_out 		<= `ALL_ZERO_2BYTE;
+			rate_out 		<= 16'h0000;
 		end
 		else begin
 			case(state)
@@ -92,30 +92,23 @@
 					pid_active				<= `FALSE;
 					pid_complete			<= `TRUE;
 				end
-				STATE_EXTEND_32BIT: begin
-					pid_active 				<= `TRUE;
-					pid_complete 			<= `FALSE;
-					angle_err_temp			<= $signed({angle_error, 	 `PADDING_ZEROS}) >>> SHIFT_TO_LSB;
-					target_rot_temp			<= $signed({target_rotation, `PADDING_ZEROS}) >>> SHIFT_TO_LSB;
-					actual_rot_temp			<= $signed({actual_rotation, `PADDING_ZEROS}) >>> SHIFT_TO_LSB;
-				end
 				STATE_CALC1: begin
 					pid_active 				<= `TRUE;
 					pid_complete 			<= `FALSE;
 					prev_rotation_error 	<= rotation_error;
-					rotation_error 			<= target_rot_temp - actual_rot_temp;
-					rotation_integral 		<= (K_I * angle_err_temp) >>> K_I_SHIFT;
+					rotation_error 			<= target_rotation - actual_rotation;
+					rotation_integral 		<= scale_val(angle_error, K_I, K_I_SHIFT);
 				end
 				STATE_CALC2: begin
 					pid_active 				<= `TRUE;
 					pid_complete 			<= `FALSE;
-					rotation_proportional 	<= (K_P * rotation_error) >>> K_P_SHIFT;
+					rotation_proportional 	<= scale_val(rotation_error, K_P, K_P_SHIFT);
 					error_change 			<= rotation_error - prev_rotation_error;
 				end
 				STATE_CALC3: begin
 					pid_active 				<= `TRUE;
 					pid_complete 			<= `FALSE;
-					rotation_derivative 	<= (K_D * error_change) >>> K_D_SHIFT;
+					rotation_derivative 	<= scale_val(error_change, K_D, K_D_SHIFT);
 				end
 				STATE_CALC4: begin
 					pid_active 				<= `TRUE;
@@ -126,16 +119,16 @@
 					pid_active 				<= `TRUE;
 					pid_complete 			<= `TRUE;
 					if(rotation_total < RATE_MIN)
-						rate_out 			<= RATE_MIN[`BITS_EXTRACT-1:0];
+						rate_out 			<= RATE_MIN;
 					else if(rotation_total > RATE_MAX)
-						rate_out 			<= RATE_MAX[`BITS_EXTRACT-1:0];
+						rate_out 			<= RATE_MAX;
 					else
-						rate_out 			<= rotation_total[`BITS_EXTRACT-1:0];
+						rate_out 			<= rotation_total;
 				end
 				default: begin
 					pid_active 				<= `FALSE;
 					pid_complete 			<= `FALSE;
-					rate_out 				<= `ALL_ZERO_2BYTE;
+					rate_out 				<= 16'h0000;
 				end
 			endcase
 		end
@@ -150,12 +143,9 @@
 			case(state)
 				STATE_WAIT: begin
 					if(start_flag)
-						next_state 	= STATE_EXTEND_32BIT;
+						next_state 	= STATE_CALC1;
 					else
 						next_state	= STATE_WAIT;
-				end
-				STATE_EXTEND_32BIT: begin
-					next_state		= STATE_CALC1;
 				end
 				STATE_CALC1: begin
 				   next_state		= STATE_CALC2;
@@ -181,4 +171,24 @@
 			endcase
 		end
 	end
+	
+function signed [15:0] scale_val;
+	input reg signed [15:0] val;
+	input reg signed [15:0] k_mult;
+	input reg signed [15:0] k_div;
+	
+	reg signed 		 [31:0] val_32;
+	reg signed 		 [31:0] k_mult_32;
+	reg signed 		 [31:0] k_div_32;
+	reg signed 		 [31:0] scaled;
+	
+	begin
+		//cast values to the 32 bits
+		val_32 		= $signed({val, 16'b0}) >>> 16;
+		
+		scaled		= (val_32 * k_mult) >>> k_div;
+		
+		scale_val 	= $signed({scaled[31], scaled[14:0]});
+	end
+endfunction
 endmodule
